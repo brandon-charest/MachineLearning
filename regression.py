8ff1bd6bc1cb1839f4149d8a2eb536c33054e3c5
Brandon Charest <b.charest019@gmail.com>
2019-04-13 11:39:34 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import quandl as q
-import math
+import math, datetime
 import numpy as np
 from sklearn import preprocessing, svm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = q.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -17,21 +21,43 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
-print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 x = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
-
 x = preprocessing.scale(x)
+x = x[:-forecast_out]
+x_lately = x[-forecast_out:]
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y= np.array(df['label'])
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-clf = svm.SVR()
+clf = LinearRegression()
 clf.fit(x_train, y_train)
 accuracy = clf.score(x_test, y_test)
 
-print(accuracy)
+forecast_set = clf.predict(x_lately)
+
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+oneday = 86400
+next_unix = last_unix + oneday
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += oneday
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
