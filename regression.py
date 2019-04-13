f46f8ad70bc52e00f7947b6d60c8ab6cd7d20c69
Brandon Charest <b.charest019@gmail.com>
2019-04-13 11:18:06 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import quandl as q
 import math
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 df = q.get('WIKI/GOOGL')
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -13,7 +17,21 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
 
-print(df.head())
+x = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+
+x = preprocessing.scale(x)
+y= np.array(df['label'])
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+clf = svm.SVR()
+clf.fit(x_train, y_train)
+accuracy = clf.score(x_test, y_test)
+
+print(accuracy)
