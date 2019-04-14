dece34c0fe6c53518d11c61d0bb4bf6311d80709
Brandon Charest <b.charest019@gmail.com>
2019-04-14 19:29:20 -0400

    regression2

--- regression2.py
+++ regression2.py
@@ -2,12 +2,24 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
-#dummy data
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+def create_dataset(size, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+
+    for i in range(size):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 def best_fit_slope_and_intercept(xs, ys):
     m = ( ((mean(xs) * mean(ys)) - mean(xs * ys)) / 
@@ -16,6 +28,18 @@ def best_fit_slope_and_intercept(xs, ys):
     b = mean(ys) - m*(mean(xs))
     return m, b
 
+def squared_error(ys_orig, ys_line):
+    return sum((ys_line - ys_orig)**2)
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for ys in ys_orig]
+    squared_error_rgr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+
+    return 1 - (squared_error_rgr / squared_error_y_mean)
+
+xs, ys = create_dataset(100, 40, 1, correlation='pos')
+
 m, b = best_fit_slope_and_intercept(xs, ys)
 
 regression_line = [(m*x)+b for x in xs]
@@ -23,9 +47,10 @@ regression_line = [(m*x)+b for x in xs]
 predict_x = 8
 predict_y = (m*predict_x)+b
 
+r_squared = coefficient_of_determination(ys, regression_line)
+
 
 plt.scatter(xs,ys)
-plt.scatter(predict_x, predict_y, color='g')
 plt.plot(xs,regression_line)
 plt.show()
 
