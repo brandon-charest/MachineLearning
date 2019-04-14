70733fb2c53922aa7144e05ebbca74044286233a
Brandon Charest <b.charest019@gmail.com>
2019-04-14 19:29:29 -0400

    fixed regression

--- regression.py
+++ regression.py
@@ -21,14 +21,14 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_CHG', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 x = np.array(df.drop(['label'], 1))
 x = preprocessing.scale(x)
-x = x[:-forecast_out]
 x_lately = x[-forecast_out:]
+x = x[:-forecast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
